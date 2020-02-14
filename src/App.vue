e29e8d496059a32a0b086d917ff04282e4f65f2a
Iago Cavalcante <iagoangelimc@gmail.com>
2020-02-14 01:37:20 -0300

    init: primeiro commit dos brothers

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,60 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Contador de Estalecas - BBB20</h1>
+    <ul>
+      <li v-for="brother in orderedBrothers()" :key="brother.id">
+        <div class="brother__pic">
+          <span v-if="brother.status === 'OUT'" :style="isOut(brother)"></span>
+          <img :src="brother.picture" alt="">
+        </div>
+        <div class="brother__info">
+          <p>
+            Nome: {{brother.name}}
+          </p>
+          <p>
+            Estalecas: {{brother.balance}}
+          </p>
+          <p>
+            Grupo: {{brother.group}}
+          </p>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: () => ({
+    brothers: [],
+    isMounted: false
+  }),
+  methods: {
+    isOut (brother) {
+      return {
+        backgroundColor: brother.status === 'OUT' ? '#fefefeA6' : '',
+        width: '15em',
+        height: '15em',
+        position: 'absolute',
+        borderRadius: '50%'
+      }
+    },
+    orderedBrothers () {
+      return this.brothers.sort((a, b) => (-1 * a.balance) - (-1* b.balance))
+    }
+  },
+  created () {
+    this.isMounted = true
+    axios.get(`${process.env.VUE_APP_HOST}/bbb`)
+      .then(response => {
+        this.brothers = response.data
+      })
+      .catch(error => {
+        console.log(error)
+      })
   }
 }
 </script>
@@ -21,8 +64,34 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+* {
+  color: white;
+  background-color: #2c3e50;
+}
+
+h1 {
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  display: grid;
+  grid-template-columns: auto auto auto;
+  text-align: center;
+}
+
+li {
+  grid-template-rows: auto auto;
+}
+
+img {
+  background-color: transparent;
+  width: 15em;
+  height: 15em;
+  border-radius: 50%;
 }
 </style>
